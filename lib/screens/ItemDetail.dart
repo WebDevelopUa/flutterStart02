656d4ed7678f2126fcaa60db9aae8799d72f8597
Serge M <sergii.mysak@vantino.ch>
2022-02-02 17:57:08 +0200

    Add safe area

--- lib/screens/ItemDetail.dart
+++ lib/screens/ItemDetail.dart
@@ -6,110 +6,113 @@ class ItemDetail extends StatelessWidget {
 // state: item
   final String? item;
 
+  @override
   Widget build(context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Detail of : $item'),
         ),
-        body: Container(
-          child: Column(
-            children: [
-              //  # CARD 1
-              Card(
-                clipBehavior: Clip.antiAlias,
-                margin: const EdgeInsets.all(15.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.wb_sunny),
-                        title: Text('Card title: $item'),
-                        subtitle: Text(
-                          '$item ... Secondary Text',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Text(
-                          'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: [
-                          OutlinedButton(
-                            onPressed: () {
-                              debugPrint('Cancelled!');
-                            },
-                            child: const Text('Cancel'),
+        body: SafeArea(
+          child: Container(
+            child: Column(
+              children: [
+                //  # CARD 1
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  margin: const EdgeInsets.all(15.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.wb_sunny),
+                          title: Text('Card title: $item'),
+                          subtitle: Text(
+                            '$item ... Secondary Text',
+                            style:
+                                TextStyle(color: Colors.black.withOpacity(0.6)),
                           ),
-                          OutlinedButton(
-                            onPressed: () {
-                              debugPrint('Submitted!');
-                            },
-                            child: const Text('Submit'),
-                          )
-                        ],
-                      ),
-                      // Image.asset('icon/palm.png'),
-                    ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text(
+                            'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                            style:
+                                TextStyle(color: Colors.black.withOpacity(0.6)),
+                          ),
+                        ),
+                        ButtonBar(
+                          alignment: MainAxisAlignment.center,
+                          children: [
+                            OutlinedButton(
+                              onPressed: () {
+                                debugPrint('Cancelled!');
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            OutlinedButton(
+                              onPressed: () {
+                                debugPrint('Submitted!');
+                              },
+                              child: const Text('Submit'),
+                            )
+                          ],
+                        ),
+                        // Image.asset('icon/palm.png'),
+                      ],
+                    ),
                   ),
                 ),
-              ),
 
-              //  # CARD 2
-              Card(
-                clipBehavior: Clip.antiAlias,
-                margin: const EdgeInsets.all(15.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.wb_sunny),
-                        title: Text('Card title: $item'),
-                        subtitle: Text(
-                          '$item ... Secondary Text',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Text(
-                          'Greyhound divisively hello coldly wonderfully marginally far upon excluding. Greyhound divisively hello coldly wonderfully marginally far upon excluding. Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: [
-                          OutlinedButton(
-                            onPressed: () {
-                              debugPrint('Cancelled!');
-                            },
-                            child: const Text('Cancel'),
+                //  # CARD 2
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  margin: const EdgeInsets.all(15.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.wb_sunny),
+                          title: Text('Card title: $item'),
+                          subtitle: Text(
+                            '$item ... Secondary Text',
+                            style:
+                                TextStyle(color: Colors.black.withOpacity(0.6)),
                           ),
-                          OutlinedButton(
-                            onPressed: () {
-                              debugPrint('Submitted!');
-                            },
-                            child: const Text('Submit'),
-                          )
-                        ],
-                      ),
-                      // Image.asset('icon/palm.png'),
-                    ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text(
+                            'Greyhound divisively hello coldly wonderfully marginally far upon excluding. Greyhound divisively hello coldly wonderfully marginally far upon excluding. Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                            style:
+                                TextStyle(color: Colors.black.withOpacity(0.6)),
+                          ),
+                        ),
+                        ButtonBar(
+                          alignment: MainAxisAlignment.center,
+                          children: [
+                            OutlinedButton(
+                              onPressed: () {
+                                debugPrint('Cancelled!');
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            OutlinedButton(
+                              onPressed: () {
+                                debugPrint('Submitted!');
+                              },
+                              child: const Text('Submit'),
+                            )
+                          ],
+                        ),
+                        // Image.asset('icon/palm.png'),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ));
   }
--- lib/screens/ItemsList.dart
+++ lib/screens/ItemsList.dart
@@ -7,27 +7,34 @@ class ItemsList extends StatelessWidget {
         appBar: AppBar(
           title: Text('Items List'),
         ),
-        body: Container(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'dart');
-                },
-                label: Text('DART'),
-                icon: Icon(Icons.wb_cloudy_outlined),
-              ),
-              FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/typescript');
-                },
-                icon: Icon(Icons.link_rounded),
-                label: Text('TYPESCRIPT'),
-              ),
-            ],
-          ),
-        )));
+        body: SafeArea(
+          child: Container(
+              child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('icon/palm.png'),
+                  backgroundColor: Colors.transparent,
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'dart');
+                  },
+                  label: Text('DART'),
+                  icon: Icon(Icons.wb_cloudy_outlined),
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/typescript');
+                  },
+                  icon: Icon(Icons.link_rounded),
+                  label: Text('TYPESCRIPT'),
+                ),
+              ],
+            ),
+          )),
+        ));
   }
 }
